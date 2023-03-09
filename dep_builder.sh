2ec0d8fa78b121e9f6ff1d035a8290d633d51453
SourceCodeDeleted <54761251+SourceCodeDeleted@users.noreply.github.com>
2023-03-09 13:33:40 +0200

    Update dep_builder.sh

--- dep_builder.sh
+++ dep_builder.sh
@@ -1,9 +1,11 @@
 echo "[+] Start"
 
-echo "[+] Building deps"
-mkdir /root/build_dependencies
+pwd 
 
-cd /root/build_dependencies
+echo "[+] Building deps"
+mkdir ~/build_dependencies
+
+cd ~/build_dependencies
 echo "[+] clone SDL2"
 git clone https://github.com/libsdl-org/SDL.git
 cd SDL/
@@ -13,7 +15,7 @@ make
 make install 
 
 
-cd /root/build_dependencies
+cd ~/build_dependencies
 echo "[+] clone openal-soft"
 git clone https://github.com/kcat/openal-soft.git
 cd openal-soft/
@@ -22,7 +24,7 @@ cmake ..
 make 
 make install
 
-cd /root/build_dependencies
+cd ~/build_dependencies
 echo "[+] clone zlib"
 git clone https://github.com/madler/zlib.git
 cd zlib/
